73531dbae824945c87864fc448f789c6397054ba
KJohn <43681947+Nyxie12@users.noreply.github.com>
2025-08-19 09:17:53 -0400

    Leet13

---
+++ Leet13.java
@@ -0,0 +1,10 @@
+public class Leet13 {
+    public static void main(String[] args){
+        String s = "III";
+        //IV = 5    IX = 10     XL = 40     XC = 100    CD = 400    CM = 900
+        //Convert the string to an array
+        String[] rom = s.split("");
+
+
+    }
+}
